112e3f59209db8aad11997e169c80fd7855466b3
soorajs <soorajs@qburst.com>
2016-02-17 16:40:18 +0530

    authentication is working

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,11 +2,12 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-  	ActionCable.server.broadcast 'room_channel', message: render_message(message)
+    ActionCable.server.broadcast 'room_channel', { message: render_message(message),
+                                                   user: message.user }
   end
   
   private
   def render_message(message)
-  	ApplicationController.renderer.render(partial: 'messages/message', locals: { message: message })
+    ApplicationController.renderer.render(partial: 'messages/message', locals: { message: message })
   end
 end
